df81662b5061ea1e0107d3fd2c7e810a2ee36560
DinaSami <dina.faur@yahoo.com>
2021-04-29 10:11:20 +0300

    lab3 done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-
-
 import React from 'react';
 import Footer from './component/Footer';
 import Main from './component/Main';
 import Header from './component/Header';
+import data from './assets/data.json';
+import SelectedBeast from './component/SelectedBeast';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class App extends React.Component{
-  render(){
-    return(
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      element: data,
+      show:false,
+      obj:{}
+    };
+  }
+  showElement= (event)=>{
+    this.setState({
+      show:true,
+      obj:event
+    });
+    console.log(event);
+  }
+  dontShowElem= ()=>{
+    this.setState({
+      show:false,
+    });
+  }
+  render() {
+    return (
       <div>
-        <Header/>
-        <Main/>
-        <Footer/>
+        <Header />
+        <Main data={this.state.element} sowModal={this.showElement}/>
+        <SelectedBeast show={this.state.show} dontShow={this.dontShowElem} obj={this.state.obj}/>
+        <Footer />
       </div>
     );
   }
 }
+
 export default App;
+
--- src/component/Main.js
+++ src/component/Main.js
@@ -1,16 +1,31 @@
-'use strict';
+
 
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CardGroup from 'react-bootstrap/CardGroup';
-import Group from './Group';
-
+import HornedBeast from './HornedBeast';
 
 class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elementData: this.props.data
+    };
+  }
+
   render() {
-    return <CardGroup id='group'>
-      <Group />
-    </CardGroup>;
+    let arraElment = this.state.elementData.map(element => {
+      return (
+        <HornedBeast
+          imgUrl={element.image_url}
+          name={element.title}
+          description={element.description}
+          sowModal={this.props.sowModal}
+        />
+      );
+    });
+    return (<CardGroup id='group'>{arraElment}</CardGroup>);
+
   }
 }
 export default Main;
